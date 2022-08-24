d0aca158013230c167913cbc614b34e40e6e7ec9
Justin Peters <JMarvel525@gmail.com>
2022-08-24 16:10:00 -0700

    add more launch seed data
    Co-authored-by: Ashe Urban <ashe@goldentongue.com>

--- LaunchApi/Models/LaunchApiContext.cs
+++ LaunchApi/Models/LaunchApiContext.cs
@@ -22,7 +22,22 @@ namespace LaunchApi.Models
                 new Launch { LaunchId = 7, Mission="To the Black", Location= "Vandenberg SFB", DaysToLaunch=18 },
                 new Launch { LaunchId = 8, Mission="Soyuz MS-22", Location= "Baikonur Cosmodrome", DaysToLaunch=29 },
                 new Launch { LaunchId = 9, Mission="NROL-91", Location= "Vandenberg SFB", DaysToLaunch=32 },
-                new Launch { LaunchId = 10, Mission="The Owl Spreads Its Wings", Location= "Rocket Lab Launch Complex", DaysToLaunch=30 }
+                new Launch { LaunchId = 10, Mission="The Owl Spreads Its Wings", Location= "Rocket Lab Launch Complex", DaysToLaunch=32 },
+                new Launch { LaunchId = 11, Mission="SES-20 & SES-21", Location= "Cape Canaveral", DaysToLaunch=32 },
+                new Launch { LaunchId = 12, Mission="Oceansat-3 (EOS-6)", Location= "FLP, Satish Dhawan Space Centre, India", DaysToLaunch=32 },
+                new Launch { LaunchId = 13, Mission="Good Luck, Have Fun", Location= "Cape Canaveral SFS", DaysToLaunch=32 },
+                new Launch { LaunchId = 14, Mission="CRS2 NG-18 (Cygnus)", Location= "Rocket Lab Launch Complex", DaysToLaunch=32 },
+                new Launch { LaunchId = 15, Mission="O3b mPower-1", Location= "Cape Canaveral KSC", DaysToLaunch=52 },
+                new Launch { LaunchId = 16, Mission="O3b mPower-2", Location= "Rocket Lab Launch Complex", DaysToLaunch=52},
+                new Launch { LaunchId = 17, Mission="Crew-5", Location= "Kennedy Space Center", DaysToLaunch=52},
+                new Launch { LaunchId = 18, Mission="SES-18 & SES-19", Location= "Cape Canaveral", DaysToLaunch=52},
+                new Launch { LaunchId = 19, Mission="JPSS-2", Location= "Vandenberg SFB", DaysToLaunch=72},
+                new Launch { LaunchId = 20, Mission="Transporter-6", Location= "Cape Canaveral", DaysToLaunch=82},
+                new Launch { LaunchId = 21, Mission="SWOT", Location= "Vandenberg SFB", DaysToLaunch=82},
+                new Launch { LaunchId = 22, Mission="USSF-67", Location= "Kennedy Space Center", DaysToLaunch=82},
+                new Launch { LaunchId = 23, Mission="Polaris Dawn", Location= "Kennedy Space Center", DaysToLaunch=100},
+                new Launch { LaunchId = 24, Mission="HawkEye 360 Flight 1/3", Location= "Rocket Lab LC-2", DaysToLaunch=100},
+                new Launch { LaunchId = 25, Mission="PACE", Location= "Cape Canveral", DaysToLaunch=100}
             );
         }
 
